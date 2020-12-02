561ba19133a1aa1c37373a9adaec15ce50d7fec1
grantbrooks18 <grantbrooks18@gmail.com>
2020-12-02 16:03:42 -0500

    Split hide and fight functions

--- RobotWars/robots/Juan.c
+++ RobotWars/robots/Juan.c
@@ -8,17 +8,19 @@ void juan_actions();
 void juan_hide();
 void juan_find();
 void juan_chase();
-void Juan_fight();
+void juan_fight(int radar_top, int radar_bottom, int lock);
 int mode_select();
-int case_select(int mode_val);
+int case_select();
 void case_execute(int juan_case);
-char statusMessage[100];
+char statusMessage[50];
 
 void juan_setup(){
+
     AddSensor(0,SENSOR_RADAR,40,40,RADAR_MAX_RANGE);
     AddSensor(1, SENSOR_RADAR,90,40,RADAR_MAX_RANGE);
     AddSensor(2,SENSOR_RANGE, 0, 0,RANGE_MAX_RANGE);
     AddSensor(3, SENSOR_RANGE, 180,0,RANGE_MAX_RANGE);
+
 }
 
 void juan_actions() {
@@ -26,33 +28,34 @@ void juan_actions() {
     SetSensorStatus(0, 1);
     SetSensorStatus(1,1);
 
-    int mode_val = 0;
-    mode_val = mode_select();
-
     int case_val = 0;
-    case_val = case_select(mode_val);
+    case_val = case_select();
 
     case_execute(case_val);
 
 }
 
 int mode_select(){
-    if(GetGeneratorStructure() < (MAX_GENERATOR_STRUCTURE/2)){
+    if(GetGeneratorStructure() < 250){
         return 1;
     } else{
         return 0;
     }
 }
 
-int case_select(int mode_val){
+int case_select(){
     /* Case 0: Hide
      * Case 1: Search
      * Case 2: Chase
      * Case 3: Combat
      */
+    int mode_val=mode_select();
 
-    if((GetSystemEnergy(SYSTEM_SHIELDS) < (MAX_SHIELD_ENERGY/2)) && (mode_val == 0)){
+    if((GetSystemEnergy(SYSTEM_SHIELDS) < 400) && (mode_val == 0)){
         return 0;
+    }
+    if((GetSystemEnergy(SYSTEM_SHIELDS) > 400) && (mode_val == 0)){ //testing search
+        return 1;
     }
     if(((GetSensorData(1) + GetSensorData(2)) == 0) && (mode_val == 0)){
         return 1;
@@ -77,11 +80,11 @@ void case_execute(int juan_case){
         break;
 
         case 2:
-            juan_chase();
+            //juan_chase();
         break;
 
         case 3:
-            juan_fight();
+            //juan_fight();
         break;
 
         case 4:
@@ -110,17 +113,17 @@ void juan_hide(){
     SetSystemChargePriorites(juan_prios);
 
 
-    if (GetSystemEnergy(SYSTEM_LASERS) < MAX_LASER_ENERGY) {
+    if (GetSystemEnergy(SYSTEM_LASERS) < 50) {
         SetSystemChargeRate(SYSTEM_LASERS, 200);
-    } else if (GetSystemEnergy(SYSTEM_LASERS) >= MAX_LASER_ENERGY) {
+    } else if (GetSystemEnergy(SYSTEM_LASERS) >= 50) {
         SetSystemChargeRate(SYSTEM_LASERS, 0);
     }
 
-    if (GetSystemEnergy(SYSTEM_MISSILES) < MIN_MISSILE_ENERGY) {
-        if (GetSystemEnergy(SYSTEM_LASERS) <= (MIN_MISSILE_ENERGY/2)) {
-            SetSystemChargeRate(SYSTEM_MISSILES, MAX_MISSILE_CHARGE_RATE);
-        } else if (GetSystemEnergy(SYSTEM_LASERS) >=(MIN_MISSILE_ENERGY/2)) {
-            SetSystemChargeRate(SYSTEM_MISSILES, (MAX_MISSILE_CHARGE_RATE/2));
+    if (GetSystemEnergy(SYSTEM_MISSILES) < 100) {
+        if (GetSystemEnergy(SYSTEM_LASERS) <= 50) {
+            SetSystemChargeRate(SYSTEM_MISSILES, 600);
+        } else if (GetSystemEnergy(SYSTEM_LASERS) >=50) {
+            SetSystemChargeRate(SYSTEM_MISSILES, 300);
         }
     } else if (GetSystemEnergy(SYSTEM_MISSILES) >= MAX_MISSILE_ENERGY) {
         SetSystemChargeRate(SYSTEM_MISSILES, 0);
@@ -151,14 +154,19 @@ void juan_hide(){
 
 void juan_find(){
     sprintf(statusMessage,
-            "Donde estan!?");
+            "On the prowl!");
     SetStatusMessage(statusMessage);
+
+    SetSensorStatus(0, 1);
+    SetSensorStatus(1,1);
+    SetSensorStatus(2, 1);
+    SetSensorStatus(3,0);
+
     if (GetSystemEnergy(SYSTEM_LASERS) < 50) {
         SetSystemChargeRate(SYSTEM_LASERS, 500);
     } else if (GetSystemEnergy(SYSTEM_LASERS) >= 50) {
         SetSystemChargeRate(SYSTEM_LASERS, 0);
     }
-
     if (GetSystemEnergy(SYSTEM_MISSILES) < 100) {
         if (GetSystemEnergy(SYSTEM_LASERS) <= 50) {
             SetSystemChargeRate(SYSTEM_MISSILES, 600);
@@ -166,130 +174,117 @@ void juan_find(){
     } else if (GetSystemEnergy(SYSTEM_MISSILES) >= 100) {
         SetSystemChargeRate(SYSTEM_MISSILES, 0);
     }
-
-    SetSystemChargeRate(SYSTEM_SHIELDS, 500);
-
-    SetSensorStatus(0, 1);
-    SetSensorStatus(1,1);
-    SetSensorStatus(2, 1);
-    SetSensorStatus(3,0);
+    if(GetSystemEnergy(SYSTEM_SHIELDS)<MAX_SHIELD_ENERGY){
+        SetSystemChargeRate(SYSTEM_SHIELDS, 500);
+    } else {SetSystemChargeRate(SYSTEM_SHIELDS, 0);}
 
     SetMotorSpeeds(100, 100);
 
-    int rad_top=GetSensorData(0);
-    int rad_bottom=GetSensorData(1);
+    int radar_top=GetSensorData(0);
+    int radar_bottom=GetSensorData(1);
     int front_d=GetSensorData(2);
-    int lock;
+    int lock=0;
 
-
-    if(rad_top==1){
-        SetMotorSpeeds(80, 100 );
+    if(radar_top==1){
+        SetMotorSpeeds(70, 100 );
         lock++;
     }
 
-    if(rad_bottom==1){
-        SetMotorSpeeds(100, 80);
+    if(radar_bottom==1){
+        SetMotorSpeeds(100, 70);
         lock++;
-
+    }
+    //combat function
+    if(lock>0){
+        juan_fight(radar_top,radar_bottom,lock);
     }
 
-
+    if((front_d<80)&&(radar_bottom==0)&&(radar_top==0)){
 
         GPS_INFO gpsData;
-        if(GetSystemEnergy(SYSTEM_SHIELDS) > 500){
+
+        if(GetSystemEnergy(SYSTEM_SHIELDS) > 400) {
             GetGPSInfo(&gpsData);
 
             sprintf(statusMessage,
-                    "x: %f\n y: %f\n heading: %f\n",gpsData.x,gpsData.y,gpsData.heading);
+                    "x: %f\n y: %f\n heading: %f\n", gpsData.x, gpsData.y, gpsData.heading);
             SetStatusMessage(statusMessage);
 
-            if(((gpsData.x<40)||(gpsData.x>300))&&((gpsData.y<40)||(gpsData.y>300))){ //robot in corner
+            if (((gpsData.x < 40) || (gpsData.x > 300)) && ((gpsData.y < 40) || (gpsData.y > 300))) { //robot in corner
                 sprintf(statusMessage,
-                        "Dios Mio! Corner Detected! \nHeading: %f",gpsData.heading);
+                        "Corner Detected! \nHeading: %f", gpsData.heading);
                 SetStatusMessage(statusMessage);
 
-                if((gpsData.x<40)&&((gpsData.y<40))){ //bottom left
+                if ((gpsData.x < 40) && ((gpsData.y < 40))) { //bottom left
                     sprintf(statusMessage,
-                            "Bottom Left Corner!\n Heading: %f",gpsData.heading);
+                            "Bottom Left Corner!\n Heading: %f", gpsData.heading);
                     SetStatusMessage(statusMessage);
-                    if(abs(gpsData.heading-45)>5){
-                        SetMotorSpeeds(50, -50);
-                    }
-                    else{
-                        SetMotorSpeeds(100,100);
-                    }
-
-
-                }
-
-                if((gpsData.x<40)&&((gpsData.y>300))){ //top left
-                    sprintf(statusMessage,
-                            "Top Left Corner! \n Heading: %f",gpsData.heading);
-                    SetStatusMessage(statusMessage);
-
-                    if(abs(gpsData.heading-315)<5){
-                        SetMotorSpeeds(100,-100);
-                    } else{
-                        SetMotorSpeeds(100,100);
+                    if (abs(gpsData.heading - 45) > 5) {
+                        SetMotorSpeeds(100, -100);
+                    } else {
+                        SetMotorSpeeds(100, 100);
                     }
 
                 }
 
-                if((gpsData.x>300)&&((gpsData.y>300))){ //top right
+                if ((gpsData.x < 40) && ((gpsData.y > 300))) { //top left
                     sprintf(statusMessage,
-                            "Top Right Corner!\n Heading: %f",gpsData.heading);
+                            "Top Left Corner! \n Heading: %f", gpsData.heading);
                     SetStatusMessage(statusMessage);
-                    if(abs(gpsData.heading-225)>5){
-                        SetMotorSpeeds(50, -50);
-                    }
-                    else{
-                        SetMotorSpeeds(100,100);
-                    }
 
+                    if (abs(gpsData.heading - 315) < 5) {
+                        SetMotorSpeeds(100, -100);
+                    } else {
+                        SetMotorSpeeds(100, 100);
+                    }
 
                 }
 
-                if((gpsData.x>300)&&((gpsData.y<40 ))){ //bottom right
+                if ((gpsData.x > 300) && ((gpsData.y > 300))) { //top right
                     sprintf(statusMessage,
-                            "Bottom Right Corner!\n Heading: %f",gpsData.heading);
+                            "Top Right Corner!\n Heading: %f", gpsData.heading);
                     SetStatusMessage(statusMessage);
-                    if(abs(gpsData.heading-135)>5){
-                        SetMotorSpeeds(50, -50);
-                    }
-                    else{
-                        SetMotorSpeeds(100,100);
+                    if (abs(gpsData.heading - 225) > 5) {
+                        SetMotorSpeeds(100, -100);
+                    } else {
+                        SetMotorSpeeds(100, 100);
                     }
 
+                }
+
+                if ((gpsData.x > 300) && ((gpsData.y < 40))) { //bottom right
+                    sprintf(statusMessage,
+                            "Bottom Right Corner!\n Heading: %f", gpsData.heading);
+                    SetStatusMessage(statusMessage);
+                    if (abs(gpsData.heading - 135) > 5) {
+                        SetMotorSpeeds(50, -50);
+                    } else {
+                        SetMotorSpeeds(100, 100);
+                    }
 
                 }
 
             }
         }
 
+    }
 
-    if ((GetSystemEnergy(SYSTEM_SHIELDS) > 500)&&((front_d<60)&&((GetBumpInfo() == 0x04)||(GetBumpInfo() == 0x08)))){
-        if (IsTurboOn() == 0) {
-            TurboBoost();
-        } else if (IsTurboOn() == 1) {
-            SetMotorSpeeds(-100, -100);
+}
+
+void juan_fight(int radar_top, int radar_bottom, int lock) {
+    SetSensorStatus(2, 0);
+    if((radar_bottom==1)&&(radar_top==1)){
+        if(lock>0){
+
+            if (GetSystemEnergy(SYSTEM_LASERS) >= 25) {
+                FireWeapon(WEAPON_LASER, 80);
+            }
+
+            if (GetSystemEnergy(SYSTEM_MISSILES) >= 100) {
+                FireWeapon(WEAPON_MISSILE, 80);
+            }
         }
+
+        SetMotorSpeeds(100, 50);
     }
-
-    if((rad_bottom==1)&&(rad_top==1)){
-          if(lock>0){
-
-                  if (GetSystemEnergy(SYSTEM_LASERS) > 25) {
-                      FireWeapon(WEAPON_LASER, 80);
-                  }
-
-                  if (GetSystemEnergy(SYSTEM_MISSILES) > 50) {
-                    FireWeapon(WEAPON_MISSILE, 80);
-                  }
-          }
-
-          SetMotorSpeeds(100, 50);
-    }
-
-
-
 }
