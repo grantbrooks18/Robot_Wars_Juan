d46633ae94c1d9bc1afd28ccc5b782a8d393bf73
FelipeLE <75095051+FelipeLE@users.noreply.github.com>
2020-12-07 18:04:05 -0500

    Juan

--- RobotWars/robots/Juan.c
+++ RobotWars/robots/Juan.c
@@ -11,20 +11,15 @@ void juan_actions();
 void juan_hide();
 void juan_find();
 int juan_obstacle(int *obstacle);
-void juan_chase();
 void juan_fight(int radar_top, int radar_bottom, int lock);
-int mode_select();
 int case_select();
 void case_execute(int juan_case);
-char statusMessage[50];
 
 void juan_setup(){
 
     AddSensor(0,SENSOR_RADAR,40,40,RADAR_MAX_RANGE);
     AddSensor(1, SENSOR_RADAR,90,40,RADAR_MAX_RANGE);
     AddSensor(2,SENSOR_RANGE, 0, 0,RANGE_MAX_RANGE);
-    AddSensor(3, SENSOR_RANGE, 180,0,RANGE_MAX_RANGE);
-
 }
 
 void juan_actions() {
@@ -32,43 +27,26 @@ void juan_actions() {
     SetSensorStatus(0, 1);
     SetSensorStatus(1,1);
 
-    int case_val = 0;
+    int case_val;
     case_val = case_select();
 
     case_execute(case_val);
 
 }
 
-int mode_select(){
-    if(GetGeneratorStructure() < 250){
-        return 1;
-    } else{
-        return 0;
-    }
-}
-
 int case_select(){
     /* Case 0: Hide
      * Case 1: Search
-     * Case 2: Chase
-     * Case 3: Combat
+     * Case 2: Combat
      */
-    int mode_val=mode_select();
-
     if(GetSystemEnergy(SYSTEM_SHIELDS) < 400){
         return 0;
     }
-    if(GetSystemEnergy(SYSTEM_SHIELDS) > 400){ //testing search
-        return 1;
-    }
-    if((GetSensorData(1) + GetSensorData(2)) == 0){
-        return 1;
-    }
-    if((GetSensorData(1) + GetSensorData(2)) == 1){
+    if((GetSensorData(1) + GetSensorData(2)) == 2){
         return 2;
     }
-    if((GetSensorData(1) + GetSensorData(2)) == 2){
-        return 3;
+    else{
+        return 1;
     }
 }
 
@@ -83,14 +61,9 @@ void case_execute(int juan_case){
             juan_find();
         break;
 
-        case 3:
-            //juan_fight();
+        case 2:
+            juan_fight(1,1,1);
         break;
-
-        case 4:
-
-        break;
-
     }
 
 }
@@ -100,7 +73,6 @@ void juan_hide(){
     SetSensorStatus(0, 0);
     SetSensorStatus(1,0);
     SetSensorStatus(2, 1);
-    SetSensorStatus(3,0);
 
     int front_d;
 
